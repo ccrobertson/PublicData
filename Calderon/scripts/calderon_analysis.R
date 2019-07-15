0c5dc758e0db930f30c54803797ac28779bd661a
ccrobertson <ccr5ju@virginia.edu>
2019-07-15 13:17:20 -0400

    separate and update normalization steps

--- Calderon/scripts/calderon_analysis.R
+++ Calderon/scripts/calderon_analysis.R
@@ -5,70 +5,59 @@ library(GenomicRanges)
 
 setwd(Sys.getenv('analysis'))
 
-### get peaks
-countMat = readRDS("counts_merged_peaks.rds")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ANALYZE CELL-TYPE SPECIFICITY
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+YfilteredNorm = readRDS(file="counts_filtered_ranknormalized.rds")
+sampleDF = readRDS(file="sampleDF.rds")
 
-### create smaple info data.frame
-parseSampleId = function(x) {
-  v = unlist(strsplit(x, split="_"))
-  n = length(v)
-  c = paste(v[seq(1,(n-3))], collapse="_")
-  return(c(c, v[seq((n-2),n)]))
+### rank peaks
+runAOV = function(y, x) {
+  summary(aov(y~x))[[1]][1,]
 }
-sampleDF = data.frame(t(sapply(colnames(countMat),parseSampleId)))
-names(sampleDF) <- c("type","donor","condition","build")
-sampleDF$sample_id = row.names(sampleDF)
-sampleDF$category = NA
-sampleDF$category[sampleDF$type %in% c("Myeloid_DCs","pDCs")] <- "DC"
-sampleDF$category[sampleDF$type %in% c("Monocytes")] <- "Monocyte"
-sampleDF$category[sampleDF$type %in% c("Immature_NK","Mature_NK","Memory_NK")] <- "NK"
-sampleDF$category[sampleDF$type %in% c("Bulk_B","Mem_B","Plasmablasts","Naive_B")] <- "B"
-sampleDF$category[sampleDF$type %in% c("CD8pos_T","Central_memory_CD8pos_T","Effector_CD4pos_T", "Effector_memory_CD8pos_T", "Follicular_T_Helper","Gamma_delta_T","Memory_Teffs","Memory_Tregs","Naive_CD8_T","Naive_Teffs","Naive_Tregs","Regulatory_T","Th1_precursors","Th17_precursors","Th2_precursors")] <- "T"
-head(sampleDF)
-
-
-### generate normalized countmatrix
-#create edgeR object
-y_raw <- DGEList(counts=countMat, samples=sampleDF)
-
-#filter for low cpm (note, eventhough y contains raw counts, this function filters based on cpm)
-min.count=10
-keep.exprs <- filterByExpr(y=y_raw, group=y_raw$samples$type, min.count=min.count)
-y <- y_raw[keep.exprs,, keep.lib.sizes=FALSE]
-
-#calculate normalization factors
-y <- calcNormFactors(y, method="TMM")
-
-#rank normalize TMM counts
-rankNormalize = function(x) {
-  qnorm((rank(x,na.last="keep")-0.5)/sum(!is.na(x)))
+getAOVResults = function(Y,x) {
+  aovRes = apply(Y, MARGIN=1, FUN=runAOV, x=x)
+  aovResMat = do.call("rbind", aovRes)
+  names(aovResultsMat) <- c("Df","SumSq","MeanSq","F","pvalue")
 }
-Y = cpm(y)
-Ynorm = t(apply(Y, MARGIN=1, FUN=rankNormalize))
-
-#kmeans clustering QC (should update this to be based on TMM not raw counts)
-y_kmeans = t(apply(Ynorm, MARGIN=1, FUN=function(x) {kmeans(x, centers=2)$cluster}))
-y_kmeans_summary = data.frame(cluster1=rowSums(y_kmeans==1), cluster2=rowSums(y_kmeans==2))
-keep2 = row.names(y_kmeans_summary)[y_kmeans_summary$cluster1>1 & y_kmeans_summary$cluster2>1]
-y_filtered <- y[keep2, , keep.lib.sizes=FALSE]
-Yfiltered = cpm(y_filtered)
-YfilteredNorm = t(apply(Yfiltered, MARGIN=1, FUN=rankNormalize))
 
 
-### rank peaks by cell category specificity
-getAOV = function(x) {
-  summary(aov(x~sampleDF$category))[[1]][1,]
-}
-aovResults = apply(YfilteredNorm, MARGIN=1, FUN=getAOV)
-aovResultsMat = do.call("rbind", aovResults)
-saveRDS(aovResultsMat, file="aov_results_peak_by_category.rds")
+#by broadest cell category
+aovRes.category = getAOVResults(YfilteredNorm, sampleDF[,"category"])
+saveRDS(aovRes.category, file="aov_results_peak_by_category.rds")
+
+#by T cell subtype
+t_cells = row.names(sampleDF[sampleDF$category=="T",])
+aovRes.T_cell_types = getAOVResults(YfilteredNorm[,t_cells], sampleDF[t_cells,"type"])
+saveRDS(aovRes.T_cell_types, file="aov_results_peak_by_T_subtype.rds")
+
+#aovRes.T_cell_types = apply(YfilteredNorm[,t_cells], MARGIN=1, FUN=getAOV, x=sampleDF[t_cells,"type"])
+#aovResMat.T_cell_types = do.call("rbind", aovRes.T_cell_types
+#names(aovResMat.T_cell_types) <- c("Df","SumSq","MeanSq","F","pvalue")
+#saveRDS(aovResMat.T_cell_types, file="aov_results_peak_by_T_subtype.rds")
+
+#by B cell subtype
+b_cells = row.names(sampleDF[sampleDF$category=="B",])
+aovRes.B_cell_types = getAOVResults(YfilteredNorm[,b_cells], sampleDF[b_cells,"type"])
+saveRDS(aovRes.B_cell_types, file="aov_results_peak_by_B_subtype.rds")
+
+
 pdf("calderon_analysis.pdf")
-hist(aovResultsMat[,"Pr(>F)"])
+par(mfrow=c(2,2))
+hist(aovRes.category[,"Pr(>F)"])
+hist(aovRes.T_cell_types[,"Pr(>F)"])
+hist(aovRes.B_cell_types[,"Pr(>F)"])
 dev.off()
 
-names(aovResultsMat) <- c("Df","SumSq","MeanSq","F","pvalue")
-head(aovResultsMat[order(aovResultsMat$F, decreasing=TRUE),], n=100)
 
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# VISUALIZE PEAKS AND OVERLAP WITH T1D CREDIBLE SNPS
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+YfilteredNorm = readRDS(file="counts_filtered_ranknormalized.rds")
+sampleDF = readRDS(file="sampleDF.rds")
+aovResultsMat = readRDS(file="aov_results_peak_by_category.rds")
+head(aovResultsMat[order(aovResultsMat$F, decreasing=TRUE),], n=100)
 
 ### get region info
 getRegion = function(x){
@@ -114,9 +103,6 @@ ggplot(regDF) + geom_density(aes(x=F, colour=T1D_credible, fill=T1D_credible), a
 #regDF$F ~ regDF$in_cred_set)
 dev.off()
 
-
-
-
 pdf("atacGR_heatmap.pdf")
 top100peaks = row.names(head(aovResultsMat[order(aovResultsMat$F, decreasing=TRUE),], n=100))
 top100peaksMat = YfilteredNorm[top100peaks,]
@@ -126,5 +112,74 @@ pheatmap(YfilteredNorm[regDF_overlap$region,], cluster_rows=TRUE, show_rownames=
         cluster_cols=TRUE, annotation_col=sampleDF[,c("donor","category","condition")], treeheight_row = 0, treeheight_col = 0, fontsize_row=2)
 dev.off()
 
+pdf("atacGR_T_cells_heatmap.pdf")
+top100peaksTcells = row.names(head(aovResMat.T_cell_types[order(aovResMat.T_cell_types$F, decreasing=TRUE),], n=100))
+top100peaksTcellsMat = YfilteredNorm[top100peaksTcells,]
+pheatmap(top100peaksTcellsMat, cluster_rows=TRUE, show_rownames=FALSE,show_colnames=FALSE,
+        cluster_cols=TRUE, annotation_col=sampleDF[,c("donor","type","condition")],treeheight_row = 0, treeheight_col = 0)
+dev.off()
 #summary(mcols(atacGR)$mcols.F)
 #summary(mcols(atacGR_overlap)$mcols.F)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# RUN SIMULATION OF IMMUNOCHIP OVERLAP
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+load("/nv/vol185/MEGA/release4/IMPUTED_TOPMED/meta_analysis/resultsmeta_5pcs_vcf_5pc_diff.RData")
+sites = res[,c("MarkerName","chromosome","position")]
+atacGR = GRanges( seqnames = regDF$chromosome, ranges = IRanges(start=as.numeric(regDF$start), end=as.numeric(regDF$end)), mcols = regDF)
+
+getSimulatedRes = function(seed, atacGR, atacDF) {
+
+  #define genomic ranges object based on random set of snps
+  set.seed(seed)
+  #randomDF = sites[base::sample(x=dim(sites)[1], size=dim(credDF)[1]),]
+  randomDF = sites[base::sample(x=dim(sites)[1], size=length(unique(credDF$tag))),]
+  randomGR = GRanges( seqnames = paste0("chr",randomDF$chromosome), ranges = IRanges(start=randomDF$position, end=randomDF$position+1), mcols = randomDF)
+
+  #get snps and peaks that overlap
+  randomGR_overlap = subsetByOverlaps(randomGR, atacGR)
+  atacGR_overlap = subsetByOverlaps(atacGR, randomGR)
+
+  #create indicators for snps overlapping peaks (or peaks overlapping snps)
+  randomDF$in_atac_peak = randomDF$MarkerName %in% mcols(randomGR_overlap)$mcols.MarkerName
+  atacDF$in_random_set = atacDF$region %in% mcols(atacGR_overlap)$mcols.region
+
+  #test if F statistic is different between overlapping and non overlapping regions
+  wilcox_out = wilcox.test(atacDF$F ~ atacDF$in_random_set)
+  overlap = sum(countOverlaps(atacGR, randomGR))
+  W = wilcox_out$statistic
+  pval = wilcox_out$p.value
+
+  return(c(overlap, W, pval))
+  #creat data.frames with only overlap
+  #randomDF_overlap = randomDF[randomDF$in_atac_peak==TRUE,]
+  #regionsDF_overlap = regionsDF[regionsDF$in_random_set==TRUE,]
+}
+
+simulation = list()
+for (i in 1:1000) {
+  cat(i,"\n")
+  simulation[[i]] = getSimulatedRes(i, atacGR=atacGR, atacDF=regDF)
+}
+simulationDF = as.data.frame(do.call("rbind", simulation))
+names(simulationDF) <- c("overlap","W","pval")
+
+
+tagDF = credDF[credDF$snp%in%credDF$tag,]
+
+pdf('simulations.pdf')
+#ggplot() + geom_density(aes(x=-log10(simulationDF$pval))) + geom_vline(xintercept=-log10(wilcox.test(regDF$F ~ regDF$in_cred_set)$p.value))
+ggplot() + geom_density(aes(x=simulationDF$overlap)) + geom_vline(xintercept=23)
+dev.off()
+
+# Summary of overlap
+# Twenty-three of 74 index snps (tag snps) overlap an accessible chromatin site.
+# To determine how this number compares to what we could expect by chance due to the immune-enrichment of the Immunochip,
+# we sampled 74 independent variants from the imputed immunochip genotypes 1000 times.
+# The median number of variants overlapping accessible chromatin peaks in immune cell types
+# was [insert]. Only # of 1000 simulations had more than 15 variants overlapping open chromatin.
+# In contrast, we saw no difference in frequency of overlap with muscle, adipose, or __ cell accessible sites between T1D index SNPs
+# randomly samples SNP sets.
+
+# Summary of cell-specificity of overlap
---
+++ Calderon/scripts/normalize_data.R
@@ -0,0 +1,154 @@
+library(edgeR)
+library(limma)
+library(ggplot2)
+library(Rtsne)
+
+setwd(Sys.getenv('analysis'))
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# read data
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+### get peaks
+countMat = readRDS("counts_merged_peaks.rds")
+
+### create smaple info data.frame
+parseSampleId = function(x) {
+  v = unlist(strsplit(x, split="_"))
+  n = length(v)
+  c = paste(v[seq(1,(n-3))], collapse="_")
+  return(c(c, v[seq((n-2),n)]))
+}
+sampleDF = data.frame(t(sapply(colnames(countMat),parseSampleId)))
+names(sampleDF) <- c("type","donor","condition","build")
+sampleDF$sample_id = row.names(sampleDF)
+sampleDF$category = NA
+sampleDF$category[sampleDF$type %in% c("Myeloid_DCs","pDCs")] <- "DC"
+sampleDF$category[sampleDF$type %in% c("Monocytes")] <- "Monocyte"
+sampleDF$category[sampleDF$type %in% c("Immature_NK","Mature_NK","Memory_NK")] <- "NK"
+sampleDF$category[sampleDF$type %in% c("Bulk_B","Mem_B","Plasmablasts","Naive_B")] <- "B"
+sampleDF$category[sampleDF$type %in% c("CD8pos_T","Central_memory_CD8pos_T","Effector_CD4pos_T", "Effector_memory_CD8pos_T", "Follicular_T_Helper","Gamma_delta_T","Memory_Teffs","Memory_Tregs","Naive_CD8_T","Naive_Teffs","Naive_Tregs","Regulatory_T","Th1_precursors","Th17_precursors","Th2_precursors")] <- "T"
+head(sampleDF)
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# filter low count peaks
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#create edgeR object
+Y_raw <- DGEList(counts=countMat, samples=sampleDF)
+
+#filter for low cpm (note, eventhough y contains raw counts, this function filters based on cpm)
+min.count=10
+keep.exprs <- filterByExpr(y=Y_raw, group=Y_raw$samples$type, min.count=min.count)
+Y_filtered <- Y_raw[keep.exprs,, keep.lib.sizes=FALSE]
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# normalize
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#calculate normalization factors
+Y_filtered <- calcNormFactors(Y_filtered, method="TMM")
+Y_cpm = cpm(Y_filtered)
+Y_lcpm = cpm(Y_filtered, log=TRUE)
+
+#rank normalize TMM counts
+rankNormalize = function(x) {
+  qnorm((rank(x,na.last="keep")-0.5)/sum(!is.na(x)))
+}
+Yranknorm = t(apply(Y_cpm, MARGIN=1, FUN=rankNormalize))
+
+#voom normalize
+mm = model.matrix(~0 + sampleDF$type + sampleDF$condition)
+Yvoomnorm = voom(Y_cpm, mm)
+
+pdf('rank_vs_voom_normalization.pdf')
+i = 1
+plot(Y_cpm[i,],Y_filtered$counts[i,], main=rownames(Y_cpm)[i])
+plot(Y_cpm[i,],Y_lcpm[i,], main=rownames(Y_cpm)[i])
+plot(Y_cpm[i,],Yranknorm[i,], main=rownames(Y_cpm)[i])
+plot(Y_cpm[i,], Yvoomnorm$E[i,], main=rownames(Y_cpm)[i])
+i = 100000
+plot(Y_cpm[i,],Y_filtered$counts[i,], main=rownames(Y_cpm)[i])
+plot(Y_cpm[i,],Y_lcpm[i,], main=rownames(Y_cpm)[i])
+plot(Y_cpm[i,],Yranknorm[i,], main=rownames(Y_cpm)[i])
+plot(Y_cpm[i,], Yvoomnorm$E[i,], main=rownames(Y_cpm)[i])
+dev.off()
+
+
+saveRDS(Yvoomnorm, file="counts_voomnormalized.rds")
+saveRDS(Yranknorm, file="counts_ranknormalized.rds")
+saveRDS(sampleDF, file="sampleDF.rds")
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# remove batch effect
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Yvoomnorm = readRDS(file="counts_voomnormalized.rds")
+Yranknorm = readRDS(file="counts_ranknormalized.rds")
+sampleDF = readRDS(file="sampleDF.rds")
+
+Yvoomnorm_nobatch = removeBatchEffect(Yvoomnorm$E, batch=sampleDF$donor)
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# exploratory plots
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+runMDS = function(Y, main) {
+  mds = plotMDS(Y, plot=FALSE)
+  mds_df = data.frame(x=mds$x, y=mds$y, donor=sampleDF$donor, type=sampleDF$type, category=sampleDF$category, condition=sampleDF$condition)
+  p1 = ggplot(mds_df) + geom_point(aes(x=x,y=y,colour=as.factor(donor))) + ggtitle(main)
+  p2 = ggplot(mds_df) + geom_point(aes(x=x,y=y,colour=as.factor(category))) + ggtitle(main) + facet_grid(.~condition)
+  print(p1)
+  print(p2)
+}
+
+runTSNE = function(Y, main) {
+  tsne = Rtsne(t(Y))
+  tsne_df = data.frame(tsne$Y,donor=sampleDF$donor, type=sampleDF$type, category=sampleDF$category, condition=sampleDF$condition)
+  p1 = ggplot(tsne_df) + geom_point(aes(x=X1,y=X2,colour=as.factor(donor))) + ggtitle(main)
+  p2 = ggplot(tsne_df) + geom_point(aes(x=X1,y=X2,colour=as.factor(category))) + ggtitle(main) + facet_grid(.~condition)
+  print(p1)
+  print(p2)
+}
+
+#get top 20% of variable peaks
+variances=NULL
+medians=NULL
+for (i in 1:dim(Yvoomnorm$E)[1]) {
+  variances[i] = var(Yvoomnorm$E[i,])
+  medians[i] = median(Yvoomnorm$E[i,])
+}
+top = variances > quantile(variances,probs=0.90)
+
+pdf("mds_plots_voom.pdf")
+runMDS(Yvoomnorm$E[top,], main="Before Batch Removal (MDS)")
+runMDS(Yvoomnorm_nobatch[top,], main="After Batch Removal (MDS)")
+runTSNE(Yvoomnorm$E[top,], main="Before Batch Removal (tSNE)")
+runTSNE(Yvoomnorm_nobatch[top,], main="After Batch Removal (tSNE)")
+dev.off()
+
+pdf("mds_plots_ranknorm.pdf")
+runTSNE(Yranknorm[top,], main="Before Batch Removal (tSNE)")
+dev.off()
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# kmeans QC (remove peaks with outliers)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+kmeansQC = function(Y) {
+  #input: Y is a matrix (each row is a peak, each column is a sample)
+  y_kmeans = t(apply(Y, MARGIN=1, FUN=function(x) {kmeans(x, centers=2)$cluster}))
+  y_kmeans_summary = data.frame(cluster1=rowSums(y_kmeans==1), cluster2=rowSums(y_kmeans==2))
+  keep = row.names(y_kmeans_summary)[y_kmeans_summary$cluster1>1 & y_kmeans_summary$cluster2>1]
+  return(Y[keep,])
+}
+
+Yranknorm_clean = kmeansQC(Yranknorm)
+Yvoomnorm_clean = kmeansQC(Yvoomnorm_nobatch)
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# save
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+### save filtered and normalized data
+saveRDS(Yranknorm_clean, file="counts_filtered_ranknormalized.rds")
+saveRDS(Yvoomnorm_clean, file="counts_filtered_voomnormalized.rds")
+saveRDS(sampleDF, file="sampleDF.rds")
